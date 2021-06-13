0627c95f6b10d202bbe622a45e31125abfa19ab4
Dominik <77927514+OverRam@users.noreply.github.com>
2021-06-13 22:35:35 +0200

    Adding methods :indexScale, getFloatPointWithTwo, ari, cl, refactor main method

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -9,31 +9,86 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String str = new String(Files.readAllBytes(Paths.get(args[0])));
-//        String str = new Scanner(System.in).nextLine();
+//        String str = new String(Files.readAllBytes(Paths.get(args[0])));
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         int sentence = str.split("[.?!]").length;
         int words = str.split(" ").length;
         int characters = (int) IntStream.range(0, str.length()).filter(i -> str.charAt(i) != ' ').count();
-        double score = 4.71 * ((characters + 0.0) / words) + 0.5 * ((words + 0.0) / sentence) - 21.43;
+        int syllables = 0;
+        int polysyllables = 0;
 
-        System.out.printf("The text is:\n%s\n\nWords: %d\nSentences: %d\nCharacters: %d\nThe score is: %.2f\n",
-                str, words, sentence, characters, (int) (score * 100) / 100.0);
+        System.out.printf("The text is:\n%s\n\nWords: %d\nSentences: %d\nCharacters: %d\n",
+                str, words, sentence, characters);
 
-        int sco = (int) Math.ceil(score);
-        String s;
 
-        if (sco == 3) {
-            s = "7-9";
-        } else if (sco < 3) {
-            s = (sco + 4) + "-" + (sco + 5);
-        } else if (sco == 13) {
-            s = "18-24";
-        } else if (sco < 13) {
-            s = (sco + 5) + "-" + (sco + 6);
-        } else {
-            s = "24";
+        String mode = "CL";//sc.nextLine();
+        double averageAge = 0;
+        switch (mode) {
+            case "ARI":
+                averageAge += ari(sentence, words, characters);
+                break;
+            case "FK":
+                averageAge += fk(sentence, words, syllables);
+                break;
+            case "SMOG":
+                averageAge += smog(polysyllables, sentence);
+                break;
+            case "CL":
+                averageAge += cl(sentence, words, characters);
+                break;
+            case "all":
+                averageAge += ari(sentence, words, characters);
+                averageAge += fk(sentence, words, syllables);
+                averageAge += smog(polysyllables, sentence);
+                averageAge += cl(sentence, words, characters);
+                break;
+            default:
+                System.out.println("Wrong choice.");
         }
+        System.out.printf("\nThis text should be understood in average by %.2f-year-olds.", averageAge);
+    }
 
-        System.out.printf("This text should be understood by %s-year-olds.", s);
+    private static double cl(double sentence, double words, double characters) {
+        double score = 0.0588 * (characters / words * 100) - 0.296 * (sentence / words * 100) - 15.8;
+        System.out.printf("Coleman–Liau index: %.2f (about %d-year-olds).",
+                getFloatPointWithTwo(score), indexScale(score));
+        return indexScale(score);
+    }
+
+    private static double smog(double polysyllables, double sentence) {
+        double score = 0;
+        return indexScale(score);
+    }
+
+    private static double fk(double sentence, double words, double syllables) {
+        double score = 0;
+        return indexScale(score);
+    }
+
+    private static double ari(double sentence, double words, double characters) {
+        double score = 4.71 * ((characters + 0.0) / words) + 0.5 * ((words + 0.0) / sentence) - 21.43;
+        System.out.printf("Automated Readability Index: %.2f This text should be understood by %d-year-olds.",
+                getFloatPointWithTwo(score), indexScale(score));
+        return indexScale(score);
+    }
+
+    private static double getFloatPointWithTwo(double score) {
+        return (int) (score * 100) / 100.0;
+    }
+
+    private static int indexScale(double score) {
+        int sco = (int) Math.round(score);
+        if (sco == 3) {
+            return 9;
+        } else if (sco < 3) {
+            return sco + 4;
+        } else if (sco == 13) {
+            return 24;
+        } else if (sco < 13) {
+            return sco + 6;
+        } else {
+            return 24;
+        }
     }
 }
